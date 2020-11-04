9b9d888580c26b24b1fecb5dd4f3ef822f075381
Carlos De la O <juan.olopez@roatech.com.mx>
2020-11-04 12:11:06 -0600

    first commit

---
+++ src/test/java/mx/com/roatech/recipeapp/RecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.roatech.recipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
