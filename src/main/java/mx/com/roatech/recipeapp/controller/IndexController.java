76042a8ca0b59095182af35bb56777e6597693a5
Carlos De la O <juan.olopez@roatech.com.mx>
2020-11-04 18:29:08 -0600

    Added index controller and index view

---
+++ src/main/java/mx/com/roatech/recipeapp/controller/IndexController.java
@@ -0,0 +1,12 @@
+package mx.com.roatech.recipeapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping({"/","/index"})
+    public String index(){
+        return ("index");
+    }
+}
